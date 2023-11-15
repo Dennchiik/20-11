f583c9498a7194c33268315de83189d65036814b
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2023-11-15 21:58:56 +0200

    Add files via upload

---
+++ 20,11(2)/20,11(2)/20,11(2).cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+int countWordOccurrences(const std::string& filename, const std::string& word) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Unable to open file!" << std::endl;
+        return -1;
+    }
+
+    std::string line;
+    int count = 0;
+
+    while (file >> line) {
+        std::transform(line.begin(), line.end(), line.begin(), ::tolower);
+        std::transform(word.begin(), word.end(), word.begin(), ::tolower);
+        if (line == word) {
+            count++;
+        }
+    }
+
+    file.close();
+    return count;
+}
+
+int main() {
+    std::string filename = "text.txt"; 
+    std::string wordToFind;
+
+    std::cout << "Enter the word to find in the file (only lowercase letters): ";
+    std::cin >> wordToFind;
+
+    int occurrences = countWordOccurrences(filename, wordToFind);
+    if (occurrences != -1) {
+        std::cout << "The word '" << wordToFind << "' appears " << occurrences << " times in the file." << std::endl;
+    }
+
+    return 0;
+}
---
+++ 20,11/20,11/20,11.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::ifstream file("text.txt"); 
+    if (!file.is_open()) {
+        std::cerr << "Unable to open file!" << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    int maxLength = 0;
+
+    while (std::getline(file, line)) {
+        int length = static_cast<int>(line.length());
+        maxLength = (length > maxLength) ? length : maxLength;
+    }
+
+    file.close();
+
+    if (maxLength > 0) {
+        std::cout << "Length of the longest line in the file: " << maxLength << " characters." << std::endl;
+    }
+    else {
+        std::cout << "The file is empty." << std::endl;
+    }
+
+    return 0;
+}
